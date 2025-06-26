fddee282f578a872a8085b35fdd093e40470a278
krigjo25 <krigjo25@gmail.com>
2025-06-26 18:35:30 +0200

    Refactor test class names and methods for clarity in APIConfig tests

--- documentation/pytest/test_APIConfig.py
+++ documentation/pytest/test_APIConfig.py
@@ -5,23 +5,32 @@ from lib.core.api_configurations import APIConfig
 from documentation.pytest.utils.data_preperation import TestCase
 
 
-class TestApi_codes(TestCase):
+class TestSuccsessfullApiCall(TestCase):
 
     "Test the APIConfig class and its methods."
     "Github : https://dummyjson.com/"
 
     REST_API_URL = "dummyjson.com/"
 
-    def test_status_code(self):
+    def test_status_code_OK(self):
         mock_response = self._data_prepearation('lib.core.api_configurations.requests.get')
 
         api = APIConfig(URL = f"{self.REST_API_URL}http/200")
         response = api.make_request()
 
         assert response.status_code == mock_response.return_value.status_code
+    
+    def test_json_response(self):
+        mock_response = self._data_prepearation('lib.core.api_configurations.requests.get')
+
+        api = APIConfig(URL = f"{self.REST_API_URL}http/200")
+        response = api.make_request()
+
+        assert response.json() == mock_response.return_value.json()
 
 class TestExceptions(TestCase):
-    
+
+    "Test the APIConfig class exceptions."
     REST_API_URL = "dummyjson.com/"
 
     def test_http_error(self):
@@ -33,16 +42,21 @@ class TestExceptions(TestCase):
             api = APIConfig(URL = f"{self.REST_API_URL}http/500")
             api.make_request()
 
-    def test_connection_error(self): pass
+    @pytest.mark.parametrize("method", ['PUT', 'PATCH', 'DELETE'])
+    def test_not_implemented_error(self, method:str):
+        with pytest.raises(NotImplementedError):
+            APIConfig(URL = f"{self.REST_API_URL}http/200").make_request(method=f'{method}')
 
-class TestApi_json(TestCase):
+    @pytest.mark.parametrize("method", ['G3T', 'P05T', 'P4TCH', 'D3L3T3'])
+    def test_value_error(self, method:str):
+        with pytest.raises(requests.exceptions.RequestException):
+            APIConfig(URL = f"{self.REST_API_URL}http/200").make_request(method=f'{method}')
 
-    REST_API_URL = "dummyjson.com/"
+    def test_connection_error(self):
+        with pytest.raises(requests.exceptions.ConnectionError):
+            APIConfig(URL = f"127.0.0.1:80").make_request()
 
-    def test_json_response(self):
-        mock_response = self._data_prepearation('lib.core.api_configurations.requests.get')
+    def test_timeout_error(self):
+        with pytest.raises(requests.exceptions.Timeout):
+            APIConfig(URL = f"{self.REST_API_URL}http/200").make_request(timeout=0.001)
 
-        api = APIConfig(URL = f"{self.REST_API_URL}http/200")
-        response = api.make_request()
-
-        assert response.json() == mock_response.return_value.json()
